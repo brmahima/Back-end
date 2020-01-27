5656d2a0c381f05e0b8c80823e9a61b266a256e5
virus0cole <mohibfak.com>
2020-01-27 19:37:38 +0200

    type of event module

---
+++ modules/typeOfEvent.js
@@ -0,0 +1,25 @@
+sequelize = require('../database/sequelize')
+const Sequelize = require('sequelize')
+//atypeOfEvent  model
+module.exports = sequelize.define('type_of_event', {
+    typOfEventId:{
+        field: "type_of_event_id",
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    nameAr:{
+        field:"name_ar",
+        type:Sequelize.STRING
+    },
+    nameEn:{
+        field:"name_en",
+        type:Sequelize.STRING
+    },
+    image:{
+        type:Sequelize.STRING
+    }
+    
+    
+})
