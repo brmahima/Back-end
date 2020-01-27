4d831934a42f3fce72b5f574c9e901a767aa1ddb
Brma <inabeel76@gmail.com>
2020-01-27 18:02:59 +0200

    added new modules

--- app.js
+++ app.js
@@ -19,12 +19,18 @@ const upload = multer({storage: storage})
 app.use(bodyParser.json())
 
 
-
+// admin module
 const Admin = require('././modules/admin')
 // vendor module
 var Vendor = require('./modules/vendor')
 // event module
 var Event = require('./modules/event')
+//artist module
+const Artist = require("./modules/artist")
+//movies module
+const Movies = require("./modules/movies")
+//sponsor module
+const Sponsor = require("./modules/sponsor")
 // Creat event
 app.post('/api/event', (req, res) => {
 
@@ -191,7 +197,7 @@ app.put('/api/vendor/:id', (req, res) => {
 })
 
 
-
+//create admin account
 app.post('/api/admin', (req, res) => {
     console.log(req.body)
     Admin.create({
@@ -210,6 +216,7 @@ app.post('/api/admin', (req, res) => {
     })
 })
 
+//login admin
 app.post('/api/adminLogin', (req, res) => {
     console.log(req.body)
 
@@ -232,6 +239,116 @@ app.post('/api/adminLogin', (req, res) => {
 
 })
 
+//create new artist
+app.post('/api/artist',upload.single('artist_image'),(req,res)=>{
+    Artist.create({
+        nameAr :req.body.name_ar,
+        nameEn :req.body.name_en,
+        image :req.file.filename
+        
+    }).then((Artist)=>{
+        res.json(Artist)
+    })
+})
+
+//getting all artist
+app.get('/api/artist', (req, res) => {
+    Artist.findAll().then((artist) => {
+        res.json(artist)
+    })
+
+})
+
+//getting artist by id
+app.get('/api/artist/:id', (req, res) => {
+    let id = req.params.id
+    Artist.findByPk(id).then((artist) => {
+        //check if exisits
+        if (artist)
+            res.json(artist)
+        else {
+            res.json({
+                'query': -1,
+                "cause": "not found"
+            })
+        }
+
+    })
+})
+
+//create new sponsor
+app.post('/api/sponsor',upload.single('sponsor_image'),(req,res)=>{
+    Sponsor.create({
+        nameAr :req.body.name_ar,
+        nameEn :req.body.name_en,
+        image :req.file.filename
+        
+    }).then((Sponsor)=>{
+        res.json(Sponsor)
+    })
+})
+
+//getting all sponsor
+app.get('/api/sponsor', (req, res) => {
+    Sponsor.findAll().then((Sponsor) => {
+        res.json(Sponsor)
+    })
+
+})
+
+//getting sponsor by id
+app.get('/api/sponsor/:id', (req, res) => {
+    let id = req.params.id
+    Sponsor.findByPk(id).then((sponsor) => {
+        //check if exisits
+        if (sponsor)
+            res.json(sponsor)
+        else {
+            res.json({
+                'query': -1,
+                "cause": "not found"
+            })
+        }
+
+    })
+})
+
+//create new movie
+app.post('/api/movie',upload.single('movie_image'),(req,res)=>{
+    Movies.create({
+        nameAr :req.body.name_ar,
+        nameEn :req.body.name_en,
+        image :req.file.filename
+        
+    }).then((Movie)=>{
+        res.json(Movie)
+    })
+})
+
+//getting all movies
+app.get('/api/movie', (req, res) => {
+    Movies.findAll().then((movie) => {
+        res.json(movie)
+    })
+
+})
+
+//getting movie by id
+app.get('/api/movie/:id', (req, res) => {
+    let id = req.params.id
+    Movies.findByPk(id).then((movie) => {
+        //check if exisits
+        if (movie)
+            res.json(movie)
+        else {
+            res.json({
+                'query': -1,
+                "cause": "not found"
+            })
+        }
+
+    })
+})
 app.listen(3000, () => {
     console.log('server is running')
 })
---
+++ modules/movies.js
@@ -0,0 +1,25 @@
+sequelize = require('../database/sequelize')
+const Sequelize = require('sequelize')
+//movies model
+module.exports = sequelize.define('movies', {
+    movieId:{
+        field: "movie_id",
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    nameAr:{
+        field:"name_ar",
+        type:Sequelize.STRING
+    },
+    nameEn:{
+        field:"name_en",
+        type:Sequelize.STRING
+    },
+    image:{
+        type:Sequelize.STRING
+    }
+    
+    
+})
