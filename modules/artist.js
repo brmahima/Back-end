fd8eb1a5ee345f3e82b405912a0619c563ee4005
virus0cole <mohibfak.com>
2020-01-28 18:06:28 +0200

    editing atributs

--- modules/artist.js
+++ modules/artist.js
@@ -2,24 +2,28 @@ sequelize = require('../database/sequelize')
 const Sequelize = require('sequelize')
 //artist model
 module.exports = sequelize.define('artist', {
-    artistId:{
-        field: "artist_id",
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
+        artistId: {
+            field: "artist_id",
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
 
-    nameAr:{
-        field:"name_ar",
-        type:Sequelize.STRING
+        nameAr: {
+            field: "name_ar",
+            type: Sequelize.STRING
+        },
+        nameEn: {
+            field: "name_en",
+            type: Sequelize.STRING
+        },
+        image: {
+            type: Sequelize.STRING
+        }
+
     },
-    nameEn:{
-        field:"name_en",
-        type:Sequelize.STRING
-    },
-    image:{
-        type:Sequelize.STRING
-    }
-    
-    
-})
+    //this is needed 
+    {
+        freezeTableName: true,
+        timestamps: false
+    })
