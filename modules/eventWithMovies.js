ad3db15b667be7f3fbbfd35790a5c14b24e56bbe
virus0cole <mohibfak.com>
2020-01-29 18:04:17 +0200

    n to n

--- modules/eventWithMovies.js
+++ modules/eventWithMovies.js
@@ -1,23 +1,17 @@
 sequelize = require('../database/sequelize')
 const Sequelize = require('sequelize')
-var Event = require('./event')
-const Movies = require("./movies")
+
 module.exports = sequelize.define('event_with_movies', {
     eventId: {
         field:"event_id",
         type: Sequelize.INTEGER,
-        references: {
-          model: Event, 
-         
-          key: 'event_id'
-        }
+        foreignKey: true,
+        primaryKey: true
       },
       moviesId: {
         field:'movie_id',
         type: Sequelize.INTEGER,
-        references: {
-          model: Sponsor, 
-          key: 'movie_id'
-        }
+        foreignKey: true,
+        primaryKey: true
       }
-},{ freezeTableName:true})
+},{ freezeTableName:true,timestamps:false})
