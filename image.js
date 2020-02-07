8f963a0119a15e154286cb48b7957814715fc17a
Brma <inabeel76@gmail.com>
2020-02-07 17:36:03 +0200

    brma

--- image.js
+++ image.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 
 })
 //to gidaq
-app.use(function(req, res, next) {
+app.use(function(req, res, next) { 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
